04ec5e9648042fcd8f5d8dac4239063bbf43d2b1
nbha-site <nbha@free.fr>
2025-04-15 19:06:43 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Script de base pour ADSN
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Bienvenue sur le site de l’ADSN !");
+  // Tu pourras ajouter ici :
+  // - Animation d'apparition
+  // - Slider dans la galerie
+  // - Gestion du formulaire de contact
+});
